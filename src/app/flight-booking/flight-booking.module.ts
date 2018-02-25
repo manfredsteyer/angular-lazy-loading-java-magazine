fc174a59879f432a53573b2bd97e00d48c2adcc6
ManfredSteyer <manfred.steyer@gmx.net>
2018-02-25 22:55:46 +0100

    sample

---
+++ src/app/flight-booking/flight-booking.module.ts
@@ -0,0 +1,39 @@
+import { TranslateModule } from '@ngx-translate/core';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
+import { FlightSearchComponent } from './flight-search/flight-search.component';
+import { FlightService } from './flight-search/flight.service';
+import { FlightCardComponent } from './flight-card/flight-card.component';
+import { PassengerSearchComponent } from './passenger-search/passenger-search.component';
+import { RouterModule } from '@angular/router';
+import { FLIGHT_BOOKING_ROUTES } from './flight-booking.routes';
+import { FlightEditComponent } from './flight-edit/flight-edit.component';
+import { FlightBookingComponent } from './flight-booking.component';
+import { OAuthModule } from 'angular-oauth2-oidc';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    OAuthModule,
+    TranslateModule,
+    SharedModule.forChild(),
+    RouterModule.forChild(FLIGHT_BOOKING_ROUTES)
+  ],
+  declarations: [
+    FlightSearchComponent,
+    FlightCardComponent,
+    PassengerSearchComponent,
+    FlightEditComponent,
+    FlightBookingComponent
+  ],
+  providers: [
+    FlightService
+  ],
+  exports: [
+    FlightSearchComponent
+  ]
+})
+export class FlightBookingModule { }
